0fed3bbc08f8e18288bb72f144a8f33879ff80a0
Edgar Garcia Munguia <edgar.garcia3@walmart.com>
2025-11-05 10:50:16 -0600

    listado-set

---
+++ src/Collections/EjemploSet.java
@@ -0,0 +1,29 @@
+package Collections;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class EjemploSet {
+    public static void main(String[] args) {
+        Set<Integer> numeros = new TreeSet<>();
+        numeros.add(5);
+        numeros.add(4);
+        numeros.add(3);
+        numeros.add(2);
+        numeros.add(1);
+        for (Integer i : numeros) {
+            System.out.println(i);
+        }
+
+        Set<String> nombres = new TreeSet<>();
+        nombres.add("Maria");
+        nombres.add("Jorge");
+        nombres.add("Solia");
+        nombres.add("Alex");
+        nombres.add("Juan");
+        for(String nombre : nombres) {
+            System.out.println(nombre);
+        }
+
+    }
+}
