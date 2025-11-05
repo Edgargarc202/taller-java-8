8642c4948e7c152b05ca3517edbcc5265ae7eeeb
Edgar Garcia Munguia <edgar.garcia3@walmart.com>
2025-11-05 12:12:45 -0600

    ejemplo wrapper

---
+++ src/wrapper_enum_generico/DemoWrapper.java
@@ -0,0 +1,12 @@
+package wrapper_enum_generico;
+
+public class DemoWrapper {
+    public static void main(String[] args) {
+        int numero = 10;
+        Integer numeroObjeto = Integer.valueOf(numero); //Boxing
+        System.out.println("Valor: " + numeroObjeto);
+
+        int sumatoria = numeroObjeto + 4; //Unboxin automatico
+        System.out.println("sumatoria: " + sumatoria);
+    }
+}
